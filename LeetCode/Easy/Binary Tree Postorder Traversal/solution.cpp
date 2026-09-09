b15ae7adb7de2321022d511f5bdcea3a34d9bfa7
ShaswatD13 <shasvedi13@gmail.com>
2026-09-10 02:31:49 +0530

    [Syncode] LeetCode/Easy/Binary Tree Postorder Traversal

---
+++ LeetCode/Easy/Binary Tree Postorder Traversal/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void shash(TreeNode* root,vector<int> &v) {
+        if(root == nullptr)
+        {
+            return;
+        }
+
+        shash(root -> left,v);
+        shash(root -> right,v);
+        v.push_back(root -> val);
+
+        return;
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        if(root == nullptr)
+        {
+            return v;
+        }
+
+        shash(root,v);
+        return v;
+    }
+};
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 | # | Problem | Platform | Difficulty | Date Solved | Time |
 |---|---------|----------|------------|-------------|------|
 | 1 | [Get Max from Stack](./GeeksForGeeks/Medium/Get%20Max%20from%20Stack) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/get-max-from-stack/1) | Medium | 08 Sept 2026 | 05:22 pm |
+| 2 | [Binary Tree Postorder Traversal](./LeetCode/Easy/Binary%20Tree%20Postorder%20Traversal) | [LeetCode](https://leetcode.com/problems/binary-tree-postorder-traversal/) | Easy | 10 Sept 2026 | 02:31 am |
